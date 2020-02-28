2f7f7ee88b1b8cdf5b2eda90e9b0b314071d557d
chane <893461089@qq.com>
2020-02-28 21:56:50 +0800

    add regexp to find city

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -18,13 +19,26 @@ func main() {
 		return
 	}
 	//fmt.Printf("%T", resp.Body)
-	all, err := ioutil.ReadAll(resp.Body)
+	all, err := ioutil.ReadAll(resp.Body)		// 返回网页源码
 	if err != nil{
 		panic(err)
 	}
-	fmt.Printf("%s ", all)
+	//fmt.Printf("%s ", all)
+	printCityList(all)
+}
+
+func printCityList(contents []byte) {
 
 
+
+	re := regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)".data-v-5e16505f[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range  matches{
+		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+
+
+	}
+	fmt.Println(len(matches))
 
 
 }
